9358ef76754cd511cec24f3860807559d1937133
LucasSantanaCode <lucas.santana.silva@outlook.com.br>
2024-12-19 22:12:32 -0300

    Adicionando Comentarios

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// CRIANDO A CLASSE
 class heroi{
     constructor(name,age,type,message){
     this.name = name
@@ -5,18 +6,20 @@ class heroi{
     this.type = type
     this.message = message
     }
-
+    // MÉTODO ATAQUE
     atack(){
         console.log(`O ${this.type} atacou usando ${this.message}`)
     }
 
 }
 
+// CRIANDO OBJETOS A PARTIR DA CLASSE
 let mago = new heroi("Ellen", 27, "Mago", "Usou Magia")
 let guerreiro = new heroi("Junior", 31, "Guerreiro", "Usou Espada")
 let monge = new heroi("Iasis", 59 ,"Monge", "Usou Artes marciais")
 let ninja = new heroi("Ellen", 33, "Ninja", "Usou Shuriken")
 
+// OBJETOS UTILIZANDO OS MÉTODOS
 mago.atack()
 guerreiro.atack()
 monge.atack()
